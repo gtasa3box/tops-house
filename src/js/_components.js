a26576eda3981532426d0a79d3fb847f8e592b11
gtasa3box <gtasa3box@gmail.com>
2022-04-01 15:56:22 +0300

    dfs

--- src/js/_components.js
+++ src/js/_components.js
@@ -4,3 +4,8 @@ $('.lang__title').click(function(){
     $($(this).data("target")).stop().slideToggle();
 });
 
+//nav
+$('.nav__title_drop').hover(function(){ 
+    $(this).toggleClass('active');
+    $($(this).data("target")).toggleClass('active');
+});
